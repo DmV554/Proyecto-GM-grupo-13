5fc5b2e39e52041900d49b5194568898e38dad54
Vicente Mediano <vicentemediano1@gmail.com>
2023-10-29 23:25:24 -0300

    Optimizacion de Paddle.java

--- BlockBreaker/core/src/com/mygdx/game/Paddle.java
+++ BlockBreaker/core/src/com/mygdx/game/Paddle.java
@@ -15,22 +15,24 @@ public class Paddle extends GameObject implements Collidable {
     	super(x,y,ancho,alto, color);
     }
 
-	public void draw(ShapeRenderer shape){
-        shape.setColor(color);
-        float x2 = x; //= Gdx.input.getX();
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) x2 =x-15;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x2=x+15; 
-       // y = Gdx.graphics.getHeight() - Gdx.input.getY(); 
-        if (x2 > 0 && x2+ancho < Gdx.graphics.getWidth()) {
-            x = x2;
-        }
-        shape.rect(x, y, ancho, alto);
-    }
+
 
     public boolean collidesWith(PingBall pp) {
         boolean intersectaX = (x + ancho >= pp.getX()-pp.getSize()) && (x <= pp.getX()+pp.getSize());
         boolean intersectaY = (y + alto >= pp.getY()-pp.getSize()) && (y <= pp.getY()+pp.getSize());
         return intersectaX && intersectaY;
+    }
+
+    public void draw(ShapeRenderer shape){
+        shape.setColor(color);
+        float x2 = x; //= Gdx.input.getX();
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) x2 =x-15;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x2=x+15;
+        // y = Gdx.graphics.getHeight() - Gdx.input.getY();
+        if (x2 > 0 && x2+ancho < Gdx.graphics.getWidth()) {
+            x = x2;
+        }
+        shape.rect(x, y, ancho, alto);
     }
 
     public void update() {
